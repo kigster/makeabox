f845562d6b9da63def43300e4c04fd2f3e7ec781
Konstantin Gredeskoul <kigster@gmail.com>
2018-01-15 13:52:52 -0800

    Upgrade to Rails 5.2 beta, reduce logging
     - also added tag to puma
     - added environment as well

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-gem 'rails'
+gem 'rails', '=5.2.0.beta2'
 
 gem 'colored2'
 gem 'jbuilder'
--- config/puma.rb
+++ config/puma.rb
@@ -1,14 +1,16 @@
 
-tag 'makabox'
-threads 4, 32 
-workers 3
+tag                        'makabox'
+threads                    4, 32 
+workers                    3
+log_requests               true
+
 preload_app!
-bind "tcp://0.0.0.0:8899"
-bind "tcp://0.0.0.0:3000"
 
-pidfile 'tmp/pids/puma.pid'
+bind                       "tcp://0.0.0.0:8899"
+bind                       "tcp://0.0.0.0:3000"
+pidfile                    'tmp/pids/puma.pid'
 
-stdout_redirect 'log/puma.stdout', 'log/puma.stderr', true
+stdout_redirect            'log/puma.stdout', 'log/puma.stderr', true
 
 on_restart do
   puts 'Restarting'
