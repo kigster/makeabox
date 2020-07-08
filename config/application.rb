dbb0e7f940a7d4f952f893c22b183d4d3032cbb5
Konstantin Gredeskoul <kigster@gmail.com>
2020-07-08 14:32:16 -0700

    Set asset host in production

--- config/application.rb
+++ config/application.rb
@@ -9,12 +9,17 @@ require 'action_view/railtie'
 require 'sprockets/railtie'
 require 'rails/test_unit/railtie'
 require 'newrelic_rpm' if Rails.env.production?
+require 'etc'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module MakeABox
+  def self.live?
+    Rails.env.production? && Etc.uname[:sysname] =~ /linux/i
+  end
+
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
