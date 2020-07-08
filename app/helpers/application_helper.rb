e0051ab5844f64dd2e9c57b2a4bbde4ef06aecba
Konstantin Gredeskoul <kigster@gmail.com>
2020-07-07 20:14:22 -0700

    Hoping to fix button helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,5 @@
+require 'etc'
+
 module ApplicationHelper
   def google?
     Rails.env.production?
@@ -5,5 +7,13 @@ module ApplicationHelper
 
   def disqus?
     Rails.env.production?
+  end
+
+  def asset_image(path, **opts)
+    uri = image_path(path, **opts)
+    if uri.start_with?('http') && Rails.env.production? && Etc.uname =~ /linux/i
+      uri.gsub! %r{^http:}, 'https:'
+    end
+    uri
   end
 end
